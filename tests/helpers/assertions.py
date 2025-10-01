86e9fee0db6c9bb7f5c3cd0fa8d9d2b395b7ceb8
Dmitrii Zavalin <zavalin.dmitry@gmail.com>
2025-10-01 19:10:54 +0000

    fixing unit tests

--- tests/helpers/assertions.py
+++
@@ -1,3 +0,0 @@
-def assert_error_contains(exception, substring):
-    """Asserts that an exception's message contains a specific substring."""
-    assert substring in str(exception.value)
--- tests/helpers/constants.py
+++
@@ -1,2 +0,0 @@
-# A common prefix for error messages from the gmsh_runner.py module
-EXPECTED_ERROR_PREFIX = "GMSH Runner Error:"
